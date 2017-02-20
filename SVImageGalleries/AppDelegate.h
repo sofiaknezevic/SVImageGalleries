dc547cb321c0b612b38d96bca320d1a717fc1602
sofiaknezevic <sofia.knezevic@gmail.com>
2017-02-20 13:50:27 -0800

    Initial commit

---
+++ SVImageGalleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SVImageGalleries
+//
+//  Created by Sofia Knezevic on 2017-02-20.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
